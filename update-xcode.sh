a3affd0088b65b8e5702830ee61056c46e3ad1a6
Dmitry Lobanov <gaussblurinc@gmail.com>
2022-02-26 21:22:40 +0300

    scripts: update-xcode mandatory git_ssh option has been added.

--- update-xcode.sh
+++ update-xcode.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+SCRIPT_DIRECTORY=$(dirname $0)
+echo "Traveling to $SCRIPT_DIRECTORY"
+cd $SCRIPT_DIRECTORY
+
 SHOULD_OPEN_XCODE=$1
 
 export MACOSX_DEPLOYMENT_TARGET=10.10  # Must match GitUp
@@ -16,10 +20,22 @@ cmake -G "Xcode" ..
 cd ../
 mv "./xcode/src/git2/sys/features.h" "./include/git2/sys/features.h"
 
+# And also add GIT_SSH and GIT_SSH_MEMORY_CREDENTIALS
+# We should add them via Package.swift, but let's do it after SPM adds plugin support.
+file="./include/git2/sys/features.h"
+
+if ! grep -q "#define GIT_SSH_MEMORY_CREDENTIALS\b" "$file" ; then
+    echo "#define GIT_SSH_MEMORY_CREDENTIALS 1" >> "$file"
+fi
+
+if ! grep -q "#define GIT_SSH\b" "$file" ; then
+    echo "#define GIT_SSH 1" >> "$file"
+fi
+
 # And open Xcode if needed.
 if [ -z "$SHOULD_OPEN_XCODE" ]
 then
-    echo "Run Xcode if you need 'xed ./xcode/libgit2.xcodeproj'"
+    echo "Run Xcode if you need 'xed $SCRIPT_DIRECTORY/xcode/libgit2.xcodeproj'"
 else
     open "./xcode/libgit2.xcodeproj"
 fi
